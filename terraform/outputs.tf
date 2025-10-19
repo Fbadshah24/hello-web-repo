a514899bc3aaa5044855cc7d2353354e53a1c4bd
Fbadshah24 <agrawalmukul300@gmail.com>
2025-10-19 05:47:23 +0530

    cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,6 +14,6 @@ output "ecr_repo_uri" {
   value = aws_ecr_repository.hello_web.repository_url
 }
 
-#output "cluster_name" {
-#  value = module.eks_cluster.name
-#}
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
